64ff12c39235a46eee6db6a35326252c026ea8a5
PhucVt2805 <phuc2805vt@phuc2805vtgmail.com>
2024-03-08 00:21:56 +0700

    Add Back Tracking

---
+++ Back Tracking/Combination_K_of_N.py
@@ -0,0 +1,12 @@
+# Sinh tổ hợp chập K của N
+N = int(input('Nhập giới hạn của dãy số: '))
+Length = int(input('Nhập độ dài tổ hợp: '))
+X = [1] * Length
+def Try(i = 1):
+    for j in range(X[i - 1] + 1, N - Length + i + 2):
+        X[i] = j
+        if(i == Length - 1):
+            print(X)
+        else:
+            Try(i+1)
+Try()
---
+++ Back Tracking/Create_Binary_Series.py
@@ -0,0 +1,14 @@
+# Bài toán sinh ra mã nhị phân khác nhau có độ dài n
+Length = int(input('Nhập độ dài xâu kí tự: '))
+X = [-1] * Length
+count = int(0)
+def Try(i = 1):
+    for j in range(2):
+        X[i - 1] = j
+        if(i == Length):
+            print(X)
+            count = count + 1
+        else:
+            Try(i + 1)
+Try()
+
